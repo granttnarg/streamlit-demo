bc642243ef2e77865eb559e30c80e16ba02080fe
granttnarg <48766845+granttnarg@users.noreply.github.com>
2025-08-14 11:50:55 +0200

    add scatterplot and filter on tab1

--- practical.py
+++ practical.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
 import base64
+import numpy as np
 
 st.write("# Worldwide Analysis of Quality of Life and Economic Factors")
-st.write("#### This app enables you to explore the relationships between poverty, life expectancy, and GDP across various countries and years. Use the panels to select options and interact with the data.")
+st.write("### This app enables you to explore the relationships between poverty, life expectancy, and GDP across various countries and years. Use the panels to select options and interact with the data.")
 
 tabs = ["Global Overview", "Country Deep Dive", "Data Explorer"]
 tab1, tab2, tab3 = st.tabs(tabs, width="stretch")
@@ -21,6 +22,51 @@ def download_csv(name,df):
   file = (f'<a href="data:file/csv;base64,{base}" download="%s.csv">Download file</a>' % (name))
 
   return file
+
+with tab1:
+  year = st.slider(
+    'Select Year',
+    min_value=min_year,
+    max_value=max_year,
+    value=(max_year)
+  )
+
+  filtered_df = df[(df['year'] == year)]
+
+  # filtered_df["GDP per capita Log"] = np.log10(df["GDP per capita"])
+  st.write("### GDPPC over Life Expectancy")
+  st.scatter_chart(data=filtered_df, x="GDP per capita", y="Life Expectancy (IHME)", x_label="GDP per Capita", height=200, y_label="Life Expectancy", color="#ffaa00", use_container_width=True)
+
+  mean_life_expectancy = filtered_df['Life Expectancy (IHME)'].mean()
+  mean_gdp_per_capita = filtered_df['GDP per capita'].mean()
+  number_of_countries = len(filtered_df['country'].unique())
+  mean_headcount_ratio_upper_mid_income_povline = filtered_df['headcount_ratio_upper_mid_income_povline'].mean()
+
+  st.dataframe(data=filtered_df, selection_mode="multi-row")
+
+  st.write("### Filtered Metrics")
+  col1, col2 = st.columns(2)
+  col3, col4 = st.columns(2)
+
+  with col1:
+      st.metric("Mean Life Expectancy", f"{mean_life_expectancy:.2f} years")
+  with col2:
+      st.metric("Mean GDP per capita", f"{mean_gdp_per_capita:.2f}")
+  with col3:
+      st.metric("Country Count", f"{number_of_countries}")
+  with col4:
+     st.metric("Mean HC Ratio Upper Mid Income Poverty Line", f"{mean_headcount_ratio_upper_mid_income_povline:.2f}")
+
+  st.markdown(
+             """
+                <style>
+                [data-testid="stMetricValue"] {
+                    font-size: 18px;
+                }
+                </style>
+              """,
+    unsafe_allow_html=True,
+  )
 
 with tab3:
   year_range = st.slider(
@@ -44,8 +90,4 @@ with tab3:
     ]
 
   st.dataframe(data=filtered_df, selection_mode="multi-row")
-
-
-
   st.markdown(download_csv('Filtered Data Frame',filtered_df),unsafe_allow_html=True)
-
